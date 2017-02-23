bef9e24ff53ba560db15975979724345b6199da6
Hromovyi Andrii <Hromovyi@i.ua>
2017-02-23 21:41:08 +0200

    add 25 task

--- array_loops_tasks/25.php
+++ array_loops_tasks/25.php
@@ -2,13 +2,28 @@
     найти максимальное и минимальное значение и поменять их местами.</h3>
 <pre>
 <?php
-$arr = array();
 
-for($i=0;$i<5;$i++){$arr[$i]=rand(0,10);}
-print_r($arr);
-echo $min = min(array_values($arr)); //минимальное значение в массиве
-echo $max = max(array_values($arr)); //максимальное значение в массиве
-
-$new_arr = array();
-
+for($i = 0; $i < 10; $i++) {
+  $arr[] = rand(0,100);
+  if($i == 0) {
+    $min = $arr[$i];
+    $min_key = $i;
+    $max = $arr[$i];
+    $max_key = $i;
+  }
+  if($min > $arr[$i]) {
+    $min = $arr[$i];
+    $min_key = $i;
+  }
+  if($max < $arr[$i]) {
+    $max = $arr[$i];
+    $max_key = $i;
+  }  
+}
+echo "До: <pre>"; print_r($arr); echo "<br>";
+$arr[$max_key] = $min;
+$arr[$min_key] = $max;
+echo "После: <pre>"; print_r($arr);
+echo "<br>Максимальное: ".$max.";   Минимальное: ".$min.";";
+?>
 
